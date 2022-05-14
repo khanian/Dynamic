e8f3e1261023261a5d14bf8806b5fd9d608cb053
khanian <khanylee@gmail.com>
2022-05-14 14:21:20 +0900

    웹화면 추가

---
+++ src/main/java/com/security/dynamic/web/dto/PostsListResponseDto.java
@@ -0,0 +1,20 @@
+package com.security.dynamic.web.dto;
+
+import com.security.dynamic.domain.posts.Posts;
+import lombok.Getter;
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
